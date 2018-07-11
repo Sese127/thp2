edbbfa6841951b9d36ebaf2fad5bed1821626d74
SERPIL COS <serpilcos@MacBook-Air-de-SERPIL.local>
2018-07-11 16:35:51 +0200

    modifs schema information

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,7 +3,7 @@
 # Table name: lessons
 #
 #  id          :uuid             not null, primary key
-#  title       :string(50)
+#  title       :string(50)       not null
 #  description :text
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
